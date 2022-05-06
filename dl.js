1130cce18bf3abfc1fb7bf893914cf151485c6be
Nakatai-0322 <nakatai0322@gmail.com>
2022-05-07 08:48:21 +0900

    Update dl.js

--- dl.js
+++ dl.js
@@ -42,7 +42,10 @@ video.on('end', () => {
     ffmpeg(`./videos/${youtubeId}.mp4`)
         .save(`./audio/${youtubeId}.mp3`);
 
-    fs.unlink(path.resolve(`/videos/${youtubeId}.mp4`));
-
     process.stdout.write('\n\n');
+
+    fs.unlink(`./videos/${youtubeId}.mp4`, (err) => {
+        if (err) throw err;
+        console.log("mp4ファイル削除完了");
+    });
 });
