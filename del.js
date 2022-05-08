7ef5e25c69aae5cb6e7783a760eb964af187dc8c
Nakatai-0322 <nakatai0322@gmail.com>
2022-05-08 20:33:06 +0900

    Update del.js

--- del.js
+++ del.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const dir = "./videos/";
+const dir2 = "./audio/"
+
+const boolofmp3 = process.argv[2]
 
 fs.readdir(dir, function(err, files) {
     if (err) {
@@ -14,3 +17,19 @@ fs.readdir(dir, function(err, files) {
         });
     });
 });
+
+if (boolofmp3 == "mp3") {
+    fs.readdir(dir2, function(err, files) {
+        if (err) {
+            throw err;
+        }
+        files.forEach(function(file) {
+            fs.unlink(`${dir2}/${file}`, function(err) {
+                if (err) {
+                    throw (err);
+                }
+                console.log(`${file}を削除済み。`);
+            });
+        });
+    });
+}
